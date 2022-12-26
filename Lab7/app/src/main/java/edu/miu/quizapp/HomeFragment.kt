3c23ced27627b2705c56d3669a68834bdf05b67f
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2022-12-26 10:16:34 +0700

    [Lab7] init project and fragments

---
+++ Lab7/app/src/main/java/edu/miu/quizapp/HomeFragment.kt
@@ -0,0 +1,23 @@
+package edu.miu.quizapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.btn_start.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.quizFragment)
+        }
+        return view
+    }
+}
