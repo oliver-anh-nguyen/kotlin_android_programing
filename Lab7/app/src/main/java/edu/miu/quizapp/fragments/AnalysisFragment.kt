b7bc0921f532e10c87f3cd43da7cd665718a3c80
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2023-01-10 13:48:33 +0700

    [Lab7] Add UI Analysis

--- Lab7/app/src/main/java/edu/miu/quizapp/fragments/AnalysisFragment.kt
+++ Lab7/app/src/main/java/edu/miu/quizapp/fragments/AnalysisFragment.kt
@@ -5,56 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import edu.miu.quizapp.R
+import edu.miu.quizapp.database.Quiz
+import edu.miu.quizapp.database.QuizDatabase
+import edu.miu.quizapp.global.BaseFragment
+import kotlinx.android.synthetic.main.fragment_analysis.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class AnalysisFragment : BaseFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AnalysisFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class AnalysisFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var questions: List<Quiz>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analysis, container, false)
+        val view = inflater.inflate(R.layout.fragment_analysis, container, false)
+        val answers = ResultFragmentArgs.fromBundle(requireArguments()).answers
+        lifecycleScope.launch(Dispatchers.IO) {
+            this@AnalysisFragment.context?.let {
+                questions = QuizDatabase(it).getQuizDao().getAllQuizzes()
+                questions.forEach{ q ->
+                    q.answer
+                }
+                view.listAnalysis.adapter = ArrayAdapter(requireContext(),
+                    android.R.layout.simple_list_item_1,
+                    popularDataAnalysis(questions, answers.toList()))
+            }
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AnalysisFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AnalysisFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun popularDataAnalysis(questions: List<Quiz>, answers: List<String>): List<String> {
+        val items = mutableListOf<String>()
+        questions.forEachIndexed { index, quiz ->
+            val listItem = String.format("%s\nYour Ans: %s\nCorrect Ans: %s", quiz.question, answers[index], quiz.answer)
+            items.add(listItem)
+        }
+        return items
     }
+
 }
